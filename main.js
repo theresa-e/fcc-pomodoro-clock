54bfa949830420e786f76130f51bdeaff8241230
Theresa Eliger <910theresa@gmail.com>
2018-11-02 12:00:58 -0700

    Worked on countdown functionality.

--- main.js
+++ main.js
@@ -2,28 +2,71 @@ $(document).ready(function () {
     // ------ Global vars -----
 
     var defaultBreak = 5;
-    var currentMin = parseInt($('#current-min').html());
+    var currentMin = parseInt($('#current-min').html()); // Get string val and parse.
+    var currentSec = parseInt($('#current-sec').html());
+    var currentBreak = parseInt($('#break-time').html());
     console.log(currentMin)
 
     // ------- Hide reset until timer starts ------ 
     $('#reset-time').hide();
+    $('#pause-time').hide();
 
-    $('#decrease-time').click(function() {
-        currentMin -= 5;
-        console.log(currentMin);
-        $('#current-min').html(currentMin);
+    // ------- Increase / decrease POMODORO TIME -----
+    $('#decrease-time').click(function () {
+        // Timer cannot be negative seconds
+        if (currentMin > 0) {
+            currentMin -= 5;
+            console.log(currentMin);
+            $('#current-min').html(currentMin);
+        }
     })
 
-    $('#increase-time').click(function() {
+    $('#increase-time').click(function () {
         currentMin += 5;
         console.log(currentMin);
         $('#current-min').html(currentMin);
     })
 
+    // ------- Start timer (reset button will fade in) -----
+    $('#start-time').click(function () {
+        var updateSeconds = function (seconds) {
+            $('#current-sec').html(seconds);
+        }
 
-    // When user clicks add time button
-    // $('#decrease-time').click(function () {
+        var counter = setInterval(function timer() {
+            if (currentMin > 0) {
+                currentSec = 60;
+            }
+            if (currentSec > 0) {
+                currentSec--;
+            }
+            updateSeconds(currentSec);
+        }, 1000)
 
-    // })
+
+    });
+
+    // ------- Reset button will set time to default 20 ------
+    $('#reset-time').click(function () {
+        currentMin = "20";
+        $('#current-min').html(currentMin);
+    })
+
+
+    // ------- Increase / decrease BREAK TIME -----
+    $('#decrease-break').click(function () {
+        // Timer cannot be negative seconds
+        if (currentBreak > 0) {
+            currentBreak -= 5;
+            console.log(currentBreak);
+            $('#break-time').html(currentBreak);
+        }
+    })
+
+    $('#increase-break').click(function () {
+        currentBreak += 5;
+        console.log(currentBreak);
+        $('#break-time').html(currentBreak);
+    })
 
 });
